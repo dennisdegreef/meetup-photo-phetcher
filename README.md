a636d294ce2d64185d3a8db89dcd58e07183e20b
Dennis de Greef <github@link0.net>
2014-12-05 14:26:14 +0100

    Finished proof of concept and added license and readme

---
+++ README.md
@@ -0,0 +1,22 @@
+Meetup photo phetcher
+=====================
+
+This repository contains a few (ugly) lines of PHP i quickly hacked togather to fetch all the photos from (Meetup.com)[http://www.meetup.com].
+
+Getting started
+===============
+
+- Copy config.dist.php to config.php
+- Edit with your own API key and target location
+- php phetch.php
+- ...
+- Profit!
+
+Remarks
+=======
+-  Yes, it is ugly
+-  Yes, it does what is should (works on my laptop
+-  Yes, you can use it as you like (see LICENSE.md)
+-  Yes, Pull requests are welcome
+
+Enjoy!
---
+++ phetch.php
@@ -0,0 +1,75 @@
+<?php
+
+include(dirname(__FILE__) . '/vendor/autoload.php');
+
+use DMS\Service\Meetup\MeetupKeyAuthClient;
+use DMS\Service\Meetup\MeetupOAuthClient;
+
+/**
+ * TODO:
+ *  - Implement caching
+ */
+
+$config = include(dirname(__FILE__) . '/config.php');
+
+$targetDirectory = $config['target_directory'];
+if(!file_exists($targetDirectory)) {
+    mkdir($targetDirectory);
+}
+
+$client = null;
+
+if(isset($config['key']) && !empty($config['key'])) {
+    $client = MeetupKeyAuthClient::factory($config);
+} else {
+    $client = MeetupOAuthClient::factory($config);
+}
+
+$events = $client->getEvents(array(
+    'member_id' => isset($config['member_id']) ? $config['member_id'] : 'self',
+    'rsvp' => 'yes',
+    'status' => 'past', // Would be cool to already fetch upcoming photos... lol
+));
+
+foreach($events as $event) {
+    $eventName = $event['name'];
+    $groupName = $event['group']['name'];
+    $timestamp = $event['time'] / 1000;
+    $date = strftime("%Y-%m-%d", $timestamp);
+    $datetime = strftime("%Y-%m-%d %H:%M:%S", $timestamp);
+
+    $groupDir = $targetDirectory . DIRECTORY_SEPARATOR . str_replace(DIRECTORY_SEPARATOR, '-', $groupName);
+    if(!file_exists($groupDir)) {
+        mkdir($groupDir);
+    }
+
+    $eventDir = $groupDir . DIRECTORY_SEPARATOR . $date;
+    if(!file_exists($eventDir)) {
+        mkdir($eventDir);
+    }
+
+    $photos = $client->getPhotos(array(
+        'event_id' => $event['id'],
+    ));
+
+    printf("%-20s -  %-40s %-40s\n", $datetime, $groupName, $eventName);
+
+    foreach($photos as $photo) {
+        $photoId = $photo['photo_id'];
+        $photoUrl = $photo['photo_link'];
+        printf("\t%-60s\n", $photoUrl);
+
+        $contents = file_get_contents($photoUrl);
+        $filename = basename($photoUrl);
+        $targetFilename = $eventDir . DIRECTORY_SEPARATOR . $filename;
+
+        if(!file_put_contents($targetFilename, $contents)) {
+            throw new Exception("Unable to save photo");
+        }
+
+        // We don't want to hit the rate limit, taking it easy
+        usleep(5000);
+    }
+
+
+}
